373e9f82694b727612e33efe4e1e0909c97c90db
k3rb3ros <k3rb3ros@k3rb3ros.com>
2013-11-18 23:52:05 -0700

    Added TourDistance(), Test() and several function templates necessary for genetic algorithm

--- tsp.cpp
+++ tsp.cpp
@@ -1,9 +1,12 @@
+#include <assert.h>
 #include <cmath>
 #include <iostream>
+#include <fstream>
 #include <vector>
 using namespace std;
 
-#define NUM_CITIES 15
+#define CITIES 15
+#define DATAFILE "cities.dat" 
 
 struct City
 {
@@ -14,24 +17,54 @@ struct City
 
 struct Tour
 {
-	City cities[NUM_CITIES];
+	City cities[CITIES];
+	int score;
 };
 
-void CleanUp(vector<Tour*> &population) //Free everything dynamically allocated
+bool PopInitialTour(Tour &tour)//create the initial tour in order 
 {
-	int len = population.size();
-
-	//Free the memory pointed to by every element in the vector then delete that element from the vector
-	for(int i=0; i<len; i++)
+	ifstream fin(DATAFILE); 
+	if(fin.good())
 	{
-	 delete population[i];
-	 population.erase(population.begin()+i);
+		for(int i=0; i<CITIES; i++) //read the data file into the initial tour
+		{
+			fin >> tour.cities[i].city;
+			fin >> tour.cities[i].x;
+			fin >> tour.cities[i].y;
+		}
+		fin.close();
+		if(fin.eof())return true;
 	}
+	return false;
 }
 
 double Distance(City A, City B)
 {
 	return hypot((double)((A.x)-(B.x)), (double)((A.y)-(B.y)));
+}
+
+double TourDistance(Tour* tour)
+{
+	double distance = 0;
+ 	for(int i=0; i<CITIES-1; i++) distance += Distance(tour -> cities[i], tour -> cities[i+1]); //get the sum of distance between all the cities
+	distance += Distance(tour -> cities[0], tour -> cities[CITIES-1]); //and the return to the original city
+	return distance;
+}
+
+int ScoreTour(Tour* tour)
+{
+	int score = 0;
+	return score;
+}
+
+void Bread(vector<Tour*> population) //breads new members of the population favoring the strong
+{
+
+}
+
+void Mutate(vector<Tour*> population) //mutates a random member of the population
+{
+
 }
 
 void PrintTour(vector<Tour*> &population, unsigned int tour_num) //Print the tours in a population
@@ -46,34 +79,73 @@ void PrintTour(vector<Tour*> &population, unsigned int tour_num) //Print the tou
 	current = population[tour_num-1];
 
 	cout << "Tour: " << tour_num << " [";
-	for(int j=0; j<NUM_CITIES; j++)
+	for(int j=0; j<CITIES; j++)
 	{
 		cout << current -> cities[j].city;
-		if(j < NUM_CITIES-1) cout << ", ";
+		if(j < CITIES-1) cout << ", ";
 	}
-	cout << "]" << endl;
+	cout << "] distance: " << TourDistance(population[tour_num-1]);
+}
+
+void Reaper(Vector<tour*> population) //Kills members of the population the single strongest always lives
+{
+
 }
 
 Tour* InitTour() //Create a new tour and set all it values as zero
 {
 	Tour* tour = new Tour;
-	for(int i=0; i<NUM_CITIES; i++)
+	for(int i=0; i<CITIES; i++)
 	{
 		tour -> cities[i].city = 0;
 		tour -> cities[i].x = 0;
 		tour -> cities[i].y = 0;
 	}
+	tour -> score = 0;
 	return tour;
+}
+
+void CleanUp(vector<Tour*> &population) //Free everything dynamically allocated
+{
+	int len = population.size();
+
+	//Free the memory pointed to by every element in the vector then delete that element from the vector
+	for(int i=0; i<len; i++)
+	{
+	 delete population[i];
+	 population.erase(population.begin()+i);
+	}
+}
+
+void Test()
+{
+	City a;
+	City b;
+
+	a.city = 1;
+	a.x = 3;
+	a.y = 5;
+	b.city = 2;
+	b.x = 9;
+	b.y = -3;
+
+	assert(Distance(a, b) == 10.0); //Test hypotenouse for a known distance
+	b.y = 3;
+	assert(Distance(a, b) == 6.324555320336759); //and one thats not so pretty
 }
 
 int main(void)
 {
+	int generation = 0;
+	Tour progenator;
 	vector<Tour*> population;
 	
-	population.push_back(InitTour()); //add a test member to population
-	
+	Test(); //if test passes continue	
+
+	//population.push_back(InitTour()); //add a test member to population
+	PopInitialTour(progenator);
+	population.push_back(&progenator);	
 	PrintTour(population, 1);
-	cout << "Distance between 1, 15 is " << Distance(population[0] -> cities[0], population[0] -> cities[14]);
 	
-	CleanUp(population);
+	//CleanUp(population);
 }
